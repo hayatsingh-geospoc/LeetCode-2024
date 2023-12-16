d70eb5e179f8a7dde1596a4ca5dd07f3946099a4
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2023-12-16 18:22:52 +0530

    LeetCode problem solved

--- easy/toBeOrToNotTobe.js
+++ easy/toBeOrToNotTobe.js
@@ -1,35 +1,23 @@
-var expect = function(val) {
-    const obj = {
-        toBe: (data)=> {
-          try{  
-            if(data === val) {
-                return true     
-            }
-            else {
-                    throw new Error('Not Equal');
-                }
-            }
-           catch(error) {
-              return error.message
-           }
-        },
-
-        notToBe: (data)=> {
-        try { 
-            if(data !== val) {
-                return true      
-            }
-            else {
-                throw new Error('Equl')
-             }
-            }
-            catch(error) {
-                return error.message;
-            }
-          },   
-        }
-        return obj;
+var expect = function (val) {
+   function  toBe(data) {
+      if (data === val) {
+        return true;
+      } else {
+        throw new Error('Not Equal');
+      }
     };
 
+     function notToBe(data) {
+      if (data !== val) {
+        return true;
+      } else {
+        throw new Error('Equl');
+      }
+    };
+  return {toBe,notToBe};
+};
 
-    console.log(expect(5).toBe(6))
+console.log(expect(5).toBe(6));
+
+
+
