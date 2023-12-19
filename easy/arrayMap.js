b18cd5ac9224c84524421372e57149eb9cdfddbd
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2023-12-19 23:54:49 +0530

    code added

--- easy/arrayMap.js
+++ easy/arrayMap.js
@@ -1,15 +1,10 @@
+let map = () => {
+  let data = {
+    name: 'hayat',
+    roll: '676',
 
+    class: '566',
+  };
 
-
-
-let map = ()=> {
-  let data = { name : "hayat",
-               roll: "676",
-            class : "566"
-            }
-
-
-
-  return data
+  return data;
 };
-
