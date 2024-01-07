bd7336f3ca417acee944eb384170750b7bfbf172
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-01-07 22:27:39 +0530

    unique ele

--- easy/unique.js
+++ easy/unique.js
@@ -1,12 +1,11 @@
+function removeUniqueElements(arr) {
+  return arr.filter((value, index, array) => {
+    return array.indexOf(value) !== array.lastIndexOf(value);
+  });
+}
 
+// Example usage:
+const originalArray = [1, 2, 3, 4, 1, 2, 5, 6, 7];
+const arrayWithDuplicates = removeUniqueElements(originalArray);
 
-let arr = ['5','3','4','3','7','5'];
-
-
-
-   let newArr = arr.filter((data)=>{
-    return data
-   })
-
-
-   console.log(newArr)
+console.log(arrayWithDuplicates);
