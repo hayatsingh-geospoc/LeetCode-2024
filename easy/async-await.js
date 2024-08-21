83cb70f80e2e88f8d8ff8c5628dc8fcf89da3753
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-08-21 23:51:23 +0530

    Update async-await.js

--- easy/async-await.js
+++ easy/async-await.js
@@ -1,8 +1,9 @@
 // write a code which shows pending reject fullfilled using async await .
 
-function PromiseReturn(status) {
+function promiseReturn(status) {
   return new Promise((rej, res) => {
     if (status == 'fullfilled') {
+      console.log('working');
       setTimeout(() => res('Promise fullfilled'), 1000);
     } else if ((status = 'Rejected')) {
       setTimeout(() => rej('Promise rejected'), 1000);
@@ -12,5 +13,11 @@ function PromiseReturn(status) {
   });
 }
 
+async function testPromise(status) {
+  try {
+    let data = await promiseReturn(status);
+    console.log(data);
+  } catch (error) {}
+}
 
-async 
+console.log(testPromise('fullfilled'));
