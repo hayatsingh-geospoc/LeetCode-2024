43d1eb632da620d5edce1a16b480a34b3657869b
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-09-03 23:32:26 +0530

    Update countVowel.js

--- easy/countVowel.js
+++ easy/countVowel.js
@@ -1,3 +1,14 @@
 let str = 'the lazy frog jump into the wolf';
+let strArr = str.split('');
 
-let vowel = ['a', 'e' , 'i' , 'o' , 'u'];
+let vowel = ['a', 'e', 'i', 'o', 'u'];
+let count = 0;
+
+let countVowel = (vowel) => {
+  strArr.forEach((element) => {
+    vowel.includes(element) && count++;
+  });
+  return count;
+};
+
+console.log(countVowel(vowel));
