3e74cb0572e523e55349097e72d002f91826dfeb
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-09-04 23:18:44 +0530

    new approach

--- easy/reverseString.js
+++ easy/reverseString.js
@@ -1,17 +1,22 @@
 let str = 'Dear hayat you are the best';
 
-let reverseString = (str) => {
-  if (str.length === 1) {
-    return str;
-  }
-  if (str.length === '') {
-    return '';
-  }
-  let arrStr = str.split(' ');
-  let reverseString = arrStr.map((data) => {
-    return data.split('').reverse().join('');
-  });
-  return reverseString;
-};
+// let reverseString = (str) => {
+//   if (str.length === 1) {
+//     return str;
+//   }
+//   if (str.length === '') {
+//     return '';
+//   }
+//   let arrStr = str.split(' ');
+//   let reverseString = arrStr.map((data) => {
+//     return data.split('').reverse().join('');
+//   });
+//   return reverseString;
+// };
 
-console.log(reverseString(str));
+// console.log(reverseString(str));
+
+// second approach
+
+let arrStr = str.split('');
+console.log(arrStr);
