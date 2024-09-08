88075510a09abcdbe0b92da57c2149d2604a2a7f
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-09-08 16:17:16 +0530

    swap numbers different appraoches

---
+++ easy/swapTwoNumbers.js
@@ -0,0 +1,17 @@
+//Swapping using third varibale.
+
+let a = 2;
+let b = 3;
+let c;
+
+c = a; // 2
+a = b; // 3
+b = c; // 2
+console.log(a, b);
+
+
+// Uisng Xor operator
+b = a ^ b; // 1
+a = a ^ b; // 3
+b = a ^ b; // 2
+console.log(a, b);
