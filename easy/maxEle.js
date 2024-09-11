1b55cc2b99b68207c3e7ba7595a588c158f63442
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-09-11 23:42:33 +0530

    Update maxEle.js

--- easy/maxEle.js
+++ easy/maxEle.js
@@ -1,6 +1,13 @@
-let maxEle = () => {
-
-
-
-    
+let maxEle = (arr) => {
+  let maxEle = arr[0];
+  arr.forEach((ele) => {
+    if (ele > arr[0]) {
+      maxEle = ele;
+    }
+  });
+  return maxEle;
 };
+
+let arr = ['3', '5', '7', '2'];
+
+console.log(maxEle(arr));
