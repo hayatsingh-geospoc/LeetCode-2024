74700751c48d2cdf42cab6eba991002110d844fd
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-09-17 23:34:30 +0530

    Update recursion.js

--- easy/recursion.js
+++ easy/recursion.js
@@ -1,10 +1,7 @@
 function factorial(n) {
   // Base case: if n is 0 or 1, return 1
   if (n === 0 || n === 1) {
-    return 1;
-
-
-    
+    return 1; 
   }
   // Recursive case: n * factorial of (n - 1)
   return n * factorial(n - 1);
