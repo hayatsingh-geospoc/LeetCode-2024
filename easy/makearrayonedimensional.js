2e263b285cc30cac2c3b6d95d94056d5571632ad
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-09-22 22:44:28 +0530

    Update makearrayonedimensional.js

--- easy/makearrayonedimensional.js
+++ easy/makearrayonedimensional.js
@@ -1,8 +1,9 @@
 let arr = [1, 3, 2, [4, 6, 8, [5, 6, 7, 2]]];
 
 const oneDimensionalArray = (arr) => {
-  return arr;
-  
+  if (Array.isArray(arr)) {
+    console.log('working');
+  }
 };
 
 console.log(oneDimensionalArray(arr));
