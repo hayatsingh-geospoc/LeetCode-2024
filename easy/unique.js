529b38229d1a949c700527ae896657a766213aa3
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-10-13 23:37:41 +0530

    Update unique.js

--- easy/unique.js
+++ easy/unique.js
@@ -1,11 +1,34 @@
-function removeUniqueElements(arr) {
-  return arr.filter((value, index, array) => {
-    return array.indexOf(value) !== array.lastIndexOf(value);
+// function removeUniqueElements(arr) {
+//   return arr.filter((value, index, array) => {
+//     return array.indexOf(value) !== array.lastIndexOf(value);
+//   });
+// }
+
+// // Example usage:
+// const originalArray = [1, 2, 3, 4, 1, 2, 5, 6, 7];
+// const arrayWithDuplicates = removeUniqueElements(originalArray);
+
+// console.log(arrayWithDuplicates);
+
+let array = [4, 5, 6, 2, 4, 6, 5, 4];
+
+// let uniqueElements = (arr) => {
+//   return arr.filter((ele, index, arrays) => {
+//     //console.log(arrays);
+//     return arrays.indexOf(ele) == array.lastIndexOf(ele);
+//   });
+// };
+
+// console.log(uniqueElements(array));
+
+function getUniqueElements(arr) {
+  return arr.filter((item, index, array) => {
+    return array.indexOf(item) === array.lastIndexOf(item);
   });
 }
 
-// Example usage:
-const originalArray = [1, 2, 3, 4, 1, 2, 5, 6, 7];
-const arrayWithDuplicates = removeUniqueElements(originalArray);
+// Example usage
+//let array = [1, 2, 3, 2, 4, 5, 6, 3, 3, 4];
+let uniqueElements = getUniqueElements(array);
 
-console.log(arrayWithDuplicates);
+console.log(uniqueElements);
