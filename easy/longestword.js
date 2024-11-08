7b5ef5f744ba7fb6cf4ab7584be39d8ec9a8bd4f
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-11-09 00:26:17 +0530

    Create longestword.js

---
+++ easy/longestword.js
@@ -0,0 +1,15 @@
+
+// Longest word in a given  sentence 
+
+
+let string = "The quick browns fox jump over the lazy dog".split(' ');
+let longestWord  = "";
+for(let i = 0;i<= string.length-1; i++){
+    if(string[i].length > longestWord.length){
+        longestWord = string[i]
+    }
+}
+
+console.log(longestWord)
+
+
