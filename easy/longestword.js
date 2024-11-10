3327c062c5ae5ffb4d12f3f15ccf4b423c8c7ffd
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-11-10 22:57:20 +0530

    palindrome/longestword

--- easy/longestword.js
+++ easy/longestword.js
@@ -2,11 +2,11 @@
 // Longest word in a given  sentence 
 
 
-let string = "The quick browns fox jump over the lazy dog".split(' ');
+let string = "The quick brown fox jump over the lazy dog".split(' ');
 let longestWord  = "";
 for(let i = 0;i<= string.length-1; i++){
     if(string[i].length > longestWord.length){
-        longestWord = string[i]
+        longestWord = string[i];
     }
 }
 
---
+++ easy/palindrome.js
@@ -0,0 +1,10 @@
+// check the string is palindrome or not
+//  example: 111 , 9009 , 151
+
+
+let palindromeNumber  =  515;
+palindromeNumber = palindromeNumber.toString();
+console.log(palindromeNumber.split(''))
+for(let i = 0; i<=palindromeNumber.length-1;i++){
+    console.log(i)
+}
