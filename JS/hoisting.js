7faba3c683ed6caeed92896484a65573ebc4dc6f
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-11-21 19:05:27 +0530

    hoisting added

---
+++ JS/hoisting.js
@@ -0,0 +1,17 @@
+// hoisting is Js defualt behaviour where all the var and funct goes top of their scopes before the execution.
+
+// temparal  deadzone
+
+
+// console.log(a);
+// const a = 5;
+
+
+
+
+let demo= ()=>{
+    console.log("working")
+};
+
+demo();
+
