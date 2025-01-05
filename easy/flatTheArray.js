0f6614e7fc10c6e43ba1d788913703789a8442e4
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2025-01-05 10:25:34 +0530

    Update flatTheArray.js

--- easy/flatTheArray.js
+++ easy/flatTheArray.js
@@ -1,34 +1,36 @@
-// let arr = [5, 6, 7, [75, 94, 9, [7, 8, 2]]];
+let arr = [5, 6, 7, [75, 94, 9, [7, 8, 2]]];
 
-// let flattenArray = (arr)=> {
-//     let arrr = []; 
-//     for (let i = 0; i < arr.length; i++) { // Corrected condition: Use < instead of <=
-//         if (Array.isArray(arr[i])) {
-//             arrr = arrr.concat(flattenArray(arr[i])); // Recursively flatten nested arrays
-//         } else {
-//             arrr.push(arr[i]); // Push non-array elements into the result
-//         }
-//     }
-//     return arrr; // Return the flattened array
-// }
-
-
-
-// let d = flattenArray(arr);
-// console.log(d); // Output: [5, 6, 7, 75, 94, 9, 7, 8, 2]
+// // let flattenArray = (arr)=> {
+// //     let arrr = []; 
+// //     for (let i = 0; i < arr.length; i++) { // Corrected condition: Use < instead of <=
+// //         if (Array.isArray(arr[i])) {
+// //             arrr = arrr.concat(flattenArray(arr[i])); // Recursively flatten nested arrays
+// //         } else {
+// //             arrr.push(arr[i]); // Push non-array elements into the result
+// //         }
+// //     }
+// //     return arrr; // Return the flattened array
+// // }
 
 
-let arr = [4,54,5,345,,345,34,534,534,,43,53]
 
+// // let d = flattenArray(arr);
+// // console.log(d); // Output: [5, 6, 7, 75, 94, 9, 7, 8, 2]
 
 let flattenArray = (arr)=>{
-    let newArr = []
-    for(let i=0; i<=arr.length-1;i++){
-        if(Array.isArray(arr))
+    let newArr = [];
+    for(let i=0;i<=arr.length-1;i++){
+        if(Array.isArray(arr[i])){
+            newArr = newArr.concat(flattenArray(arr[i]))
+        }else{
+           newArr.push(arr[i])
+        }
     }
-
+    return newArr
 };
 
 let data = flattenArray(arr);
 
 console.log(data)
+
+  
